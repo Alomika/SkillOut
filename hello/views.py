b8acbe06c24c32476e19bd022d042cf5b376f5ed
Patricijal <p.legataite@gmail.com>
2026-03-12 20:26:22 +0200

    Initialize Django project structure with basic app setup

---
+++ hello/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from datetime import datetime
+
+# Create your views here.
+from django.http import HttpResponse
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
