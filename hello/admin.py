21d8d840ce3146e33e03a68e258ef373ab8238a0
Egle Kuliavaite <ekuliavaite@gmail.com>
2026-03-14 16:59:53 +0200

    SD-95 Feature: Created models and tables for subject, student, studentsubject and subject category

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Student, Subject, StudentSubject
 
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    # Add more customizations as needed
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(StudentSubject)
+class StudentSubjectAdmin(admin.ModelAdmin):
+    list_display = ('student', 'subject', 'interest')
+    list_filter = ('interest',)
